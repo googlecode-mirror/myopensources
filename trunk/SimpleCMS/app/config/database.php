50cf55552bc2125628748f26987bf4cbe912bd1a
arzen1013 <arzen1013@197dc3fe-d32b-11dd-9aad-3f7dfcff7c77>
2009-01-15 04:55:16 +0000

    Init

---
+++ trunk/SimpleCMS/app/config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => 'test1234',
+		'database' => 'simple_cms',
+	);
+}
+?>
