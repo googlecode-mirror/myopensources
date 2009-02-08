dba139e1c4070e1f1b29af8a36866a8b50608ac2
arzen1013 <arzen1013@197dc3fe-d32b-11dd-9aad-3f7dfcff7c77>
2009-02-08 02:51:14 +0000

    update database design

--- trunk/SimpleCMS/doc/design/simple_cms_mysql_design.sql
+++ trunk/SimpleCMS/doc/design/simple_cms_mysql_design.sql
@@ -2,20 +2,46 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-CREATE SCHEMA IF NOT EXISTS `dev_simple_cms` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+CREATE SCHEMA IF NOT EXISTS `dev_simple_cms` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
 USE `dev_simple_cms`;
 
 -- -----------------------------------------------------
 -- Table `dev_simple_cms`.`article_categories`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `dev_simple_cms`.`article_categories` ;
-
 CREATE  TABLE IF NOT EXISTS `dev_simple_cms`.`article_categories` (
-  `id` INT NOT NULL AUTO_INCREMENT ,
-  `pid` INT NULL ,
-  `name` VARCHAR(45) NULL ,
-  `created` INT NULL ,
-  `modified` INT NULL ,
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `parent_id` INT(11) NOT NULL DEFAULT '0' ,
+  `lft` INT(11) NOT NULL DEFAULT '0' ,
+  `rght` INT(11) NOT NULL DEFAULT '0' ,
+  `name` VARCHAR(45) NULL DEFAULT NULL ,
+  `created` INT(11) NULL DEFAULT NULL ,
+  `modified` INT(11) NULL DEFAULT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = MyISAM;
+
+
+-- -----------------------------------------------------
+-- Table `dev_simple_cms`.`users`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `dev_simple_cms`.`users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `username` CHAR(50) NULL DEFAULT NULL ,
+  `password` CHAR(50) NULL DEFAULT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = MyISAM;
+
+
+-- -----------------------------------------------------
+-- Table `dev_simple_cms`.`articles`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `dev_simple_cms`.`articles` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `article_categories_id` INT(11) NOT NULL DEFAULT '0' ,
+  `title` VARCHAR(120) NOT NULL ,
+  `contents` TEXT NOT NULL ,
+  `photo` VARCHAR(100) NULL ,
+  `created` INT(11) NULL ,
+  `modified` INT(11) NULL ,
   PRIMARY KEY (`id`) )
 ENGINE = MyISAM;
 
