23f10a5ea58fb86ff526ecbc6bf1b4e32350b7da
arzen1013 <arzen1013@197dc3fe-d32b-11dd-9aad-3f7dfcff7c77>
2009-02-08 12:22:25 +0000

    add file validate behaviors

--- trunk/SimpleCMS/app/models/article.php
+++ trunk/SimpleCMS/app/models/article.php
@@ -1,13 +1,21 @@
 <?php
+/**
+ *
+ * File description
+ *
+ * @package    App
+ * @subpackage Model
+ * @author  John Meng (孟远螓) arzen1013@gmail.com 2009-2-8
+ * @version 1.0.0 $id$
+ */
+App::import("Vendor", "Validate_File", array('file' => 'Validate'.DS.'File.php') );
+
 class Article extends AppModel {
 
 	var $name = 'Article';
-	var $validate = array(
-		'title' => array('notempty'),
-		'contents' => array('notempty'),
-//		'photo' => array('notempty')
-	);
-
+	
+	var $actsAs = array('FileValidator');
+	
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 	var $belongsTo = array(
 			'ArticleCategory' => array('className' => 'ArticleCategories',
@@ -18,5 +26,17 @@ class Article extends AppModel {
 			)
 	);
 
+	function beforeValidate($options = array()) {
+		
+		$this->validate = array(
+			'title' => array('notempty'),
+			'contents' => array('notempty'),
+			'photo' => array(
+				'rule' => array("fileMaxsize", 2097152),
+				'message' => __("Too large", true),
+			)
+		);
+		
+	}	
 }
 ?>
