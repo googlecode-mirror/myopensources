d8039bb139ec9d1ac1f2924190abdf7c60afc0bf
arzen1013 <arzen1013@197dc3fe-d32b-11dd-9aad-3f7dfcff7c77>
2009-02-10 05:13:27 +0000

    add crop/rotate/watermark for upload image logic

--- CakeLib/vendors/Image/Lib.php
+++ CakeLib/vendors/Image/Lib.php
@@ -408,7 +408,7 @@ class Image_Lib {
 	
 
 		// Choose resizing function
-		if ($this->image_library == 'imagemagick' OR $this->image_library == 'netpbm')
+		if (in_array($this->image_library, array('imagemagick', 'netpbm', 'magickwand') ))
 		{
 			$protocol = 'image_process_'.$this->image_library;
 		
@@ -563,7 +563,25 @@ class Image_Lib {
 			case 'crop':
 		        MagickCropImage($magick_wand, $this->width, $this->height, $this->x_axis, $this->y_axis );
 				break;
-			
+			case 'rotate':
+				
+				switch ($this->rotation_angle)
+				{
+					case 90		:	$angle = 90;
+						break;
+					case 180	:	$angle = 180;
+						break;
+					case 270 	:	$angle = 270;
+						break;
+					case 'vrt'	:	$angle = 180;
+						break;
+					case 'hor'	:	$angle = 270;
+						break;
+				}
+				
+		        MagickRotateImage($magick_wand, null, $angle );
+				break;
+				
 			case 'resize':
 			default:
 		        MagickResizeImage($magick_wand, $this->width, $this->height, MW_LanczosFilter, 1.0);
@@ -747,11 +765,11 @@ class Image_Lib {
 	{	
 		// Is Image Rotation Supported?
 		// this function is only supported as of PHP 4.3
-		if ( ! function_exists('imagerotate'))
-		{
-			$this->set_error('imglib_rotate_unsupported');
-			return FALSE;
-		}
+//		if ( ! function_exists('imagerotate'))
+//		{
+//			$this->set_error('imglib_rotate_unsupported');
+//			return FALSE;
+//		}
 		
 		//  Create the image handle
 		if ( ! ($src_img = $this->image_create_gd()))
@@ -767,7 +785,11 @@ class Image_Lib {
 		$white	= imagecolorallocate($src_img, 255, 255, 255);
 
 		//  Rotate it!
-		$dst_img = imagerotate($src_img, $this->rotation_angle, $white);
+		if ( !function_exists('imagerotate') ) {
+			$dst_img = $this->imageRotate($src_img, $this->rotation_angle, $white);
+		}
+		else
+			$dst_img = imagerotate($src_img, $this->rotation_angle, $white);
 	
 		//  Save the Image
 		if ($this->dynamic_output == TRUE)
@@ -793,6 +815,91 @@ class Image_Lib {
 		
 		return true;
 	}
+	
+	// $src_img - a GD image resource
+	// $angle - degrees to rotate clockwise, in degrees
+	// returns a GD image resource
+	// USAGE:
+	// $im = imagecreatefrompng('test.png');
+	// $im = imagerotate($im, 15);
+	// header('Content-type: image/png');
+	// imagepng($im);
+	function imageRotate($src_img, $angle, $bicubic=false) {
+	 
+	   // convert degrees to radians
+	   $angle = $angle + 180;
+	   $angle = deg2rad($angle);
+	 
+	   $src_x = imagesx($src_img);
+	   $src_y = imagesy($src_img);
+	 
+	   $center_x = floor($src_x/2);
+	   $center_y = floor($src_y/2);
+	
+	   $cosangle = cos($angle);
+	   $sinangle = sin($angle);
+	
+	   $corners=array(array(0,0), array($src_x,0), array($src_x,$src_y), array(0,$src_y));
+	
+	   foreach($corners as $key=>$value) {
+	     $value[0]-=$center_x;        //Translate coords to center for rotation
+	     $value[1]-=$center_y;
+	     $temp=array();
+	     $temp[0]=$value[0]*$cosangle+$value[1]*$sinangle;
+	     $temp[1]=$value[1]*$cosangle-$value[0]*$sinangle;
+	     $corners[$key]=$temp;   
+	   }
+	  
+	   $min_x=1000000000000000;
+	   $max_x=-1000000000000000;
+	   $min_y=1000000000000000;
+	   $max_y=-1000000000000000;
+	  
+	   foreach($corners as $key => $value) {
+	     if($value[0]<$min_x)
+	       $min_x=$value[0];
+	     if($value[0]>$max_x)
+	       $max_x=$value[0];
+	  
+	     if($value[1]<$min_y)
+	       $min_y=$value[1];
+	     if($value[1]>$max_y)
+	       $max_y=$value[1];
+	   }
+	
+	   $rotate_width=round($max_x-$min_x);
+	   $rotate_height=round($max_y-$min_y);
+	
+	   $rotate=imagecreatetruecolor($rotate_width,$rotate_height);
+	   imagealphablending($rotate, false);
+	   imagesavealpha($rotate, true);
+	
+	   //Reset center to center of our image
+	   $newcenter_x = ($rotate_width)/2;
+	   $newcenter_y = ($rotate_height)/2;
+	
+	   for ($y = 0; $y < ($rotate_height); $y++) {
+	     for ($x = 0; $x < ($rotate_width); $x++) {
+	       // rotate...
+	       $old_x = round((($newcenter_x-$x) * $cosangle + ($newcenter_y-$y) * $sinangle))
+	         + $center_x;
+	       $old_y = round((($newcenter_y-$y) * $cosangle - ($newcenter_x-$x) * $sinangle))
+	         + $center_y;
+	     
+	       if ( $old_x >= 0 && $old_x < $src_x
+	             && $old_y >= 0 && $old_y < $src_y ) {
+	
+	           $color = imagecolorat($src_img, $old_x, $old_y);
+	       } else {
+	         // this line sets the background colour
+	         $color = imagecolorallocatealpha($src_img, 255, 255, 255, 127);
+	       }
+	       imagesetpixel($rotate, $x, $y, $color);
+	     }
+	   }
+	  
+	  return($rotate);
+	}	
 	
 	// --------------------------------------------------------------------
 	
