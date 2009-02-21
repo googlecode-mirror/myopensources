65545532482296646a5144b1dee53d9ce3a74ded
arzen1013 <arzen1013@197dc3fe-d32b-11dd-9aad-3f7dfcff7c77>
2009-02-21 10:36:24 +0000

    add environment class

---
+++ CakeLib/vendors/Environment/Client.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *
+ * File Desription
+ * 
+ * @package    Vendor
+ * @subpackage Environment
+ * @author  John Meng (孟远螓) arzen1013@gmail.com 2009-2-21
+ * @version 1.0.0 $id$
+ */
+
+class Environment_Client 
+{
+	function getClientIP() 
+	{
+		if(isset($_SERVER["HTTP_X_FORWARDED_FOR"]) && $_SERVER["HTTP_X_FORWARDED_FOR"] ) 
+		{
+			if($leng = strpos($_SERVER["HTTP_X_FORWARDED_FOR"], ",")) 
+			{
+				$addip = substr($_SERVER["HTTP_X_FORWARDED_FOR"],0,$leng);
+			}
+			else
+			{
+				$addip =$_SERVER["HTTP_X_FORWARDED_FOR"];
+			}
+		}
+		else
+		{
+			$addip = $_SERVER["REMOTE_ADDR"];
+		}
+		return $addip;
+
+	}
+}
