ad4772eadef741f6600dfc1c13c42616b39049a7
arzen1013 <arzen1013@197dc3fe-d32b-11dd-9aad-3f7dfcff7c77>
2009-02-21 11:02:05 +0000

    add remove multi records feature

--- SimpleCMS/app/plugins/finance/finance_app_model.php
+++ SimpleCMS/app/plugins/finance/finance_app_model.php
@@ -9,7 +9,17 @@
  * @version 1.0.0 $id$
  */
 class FinanceAppModel extends AppModel {
-
+	
+	function delIds($id_data) {
+		if (empty($id_data)) {
+			return false;
+		}
+		$ids = implode(",", $id_data);
+		$sql = "DELETE FROM ".$this->table." WHERE id IN ({$ids})";
+		$this->query($sql);
+		return true;
+	}
+	
 }
 
 ?>
