6fb889de3ff525aea6a9af4c17bcb63e122c7419
arzen1013 <arzen1013@197dc3fe-d32b-11dd-9aad-3f7dfcff7c77>
2009-02-21 10:42:29 +0000

    update beforce save behavior

--- SimpleCMS/app/plugins/finance/models/finance_category.php
+++ SimpleCMS/app/plugins/finance/models/finance_category.php
@@ -2,6 +2,8 @@
 class FinanceCategory extends FinanceAppModel {
 
 	var $name = 'FinanceCategory';
+	var $actsAs = array('Info');
+	
 	var $validate = array(
 		'category_name' => array('notempty'),
 		'active' => array('notempty')
--- SimpleCMS/app/plugins/finance/sql/finance.sql
+++ SimpleCMS/app/plugins/finance/sql/finance.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2009 年 02 月 21 日 08:32
+-- 生成日期: 2009 年 02 月 21 日 10:40
 -- 服务器版本: 5.1.30
 -- PHP 版本: 5.2.8
 
@@ -16,18 +16,18 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 /*!40101 SET NAMES utf8 */;
 
 --
--- 数据库: `dev_apf`
+-- 数据库: `dev_simple_cms`
 --
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `apf_finance`
+-- 表的结构 `finance`
 --
 
-CREATE TABLE IF NOT EXISTS `apf_finance` (
+CREATE TABLE IF NOT EXISTS `finance` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `category` int(5) DEFAULT NULL,
+  `finance_category_id` int(5) DEFAULT NULL,
   `create_date` date DEFAULT NULL,
   `amount` tinyint(4) DEFAULT NULL,
   `debit` varchar(4) DEFAULT NULL,
@@ -38,23 +38,23 @@ CREATE TABLE IF NOT EXISTS `apf_finance` (
   `groupid` varchar(11) NOT NULL DEFAULT '0',
   `userid` int(11) NOT NULL DEFAULT '0',
   `add_ip` varchar(24) DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `update_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created` int(11) NOT NULL,
+  `modified` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=134 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `apf_finance_category`
+-- 表的结构 `finance_categories`
 --
 
-CREATE TABLE IF NOT EXISTS `apf_finance_category` (
+CREATE TABLE IF NOT EXISTS `finance_categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `category_name` varchar(60) DEFAULT NULL,
   `active` varchar(8) NOT NULL DEFAULT 'new',
   `add_ip` varchar(24) DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `update_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created` int(11) NOT NULL,
+  `modified` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC AUTO_INCREMENT=9 ;
