b8e3e79408d3624519185b631f315f8baabb2429
arzen1013 <arzen1013@197dc3fe-d32b-11dd-9aad-3f7dfcff7c77>
2009-04-09 05:18:29 +0000

    add template

---
+++ trunk/WordPress/wp-content/themes/essence/index.php
@@ -0,0 +1,9 @@
+<?php get_header();?>
+
+  <!-- contents -->
+  <div id="content">此处显示  id "content" 的内容</div>
+  
+  <!-- right sidebar -->
+  <div id="r-sidebar"><?php get_sidebar(); ?></div>
+
+<?php get_footer(); ?>
